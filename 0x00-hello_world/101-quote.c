93ba447a06c67bda99961f0da01d107aeb466311
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-02-16 21:01:25 +0200

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+fprintf(stderr, "and that piece of art is useful");
+fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
