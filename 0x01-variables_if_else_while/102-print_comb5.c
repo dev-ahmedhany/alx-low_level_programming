c0392338d1cabafc7b49b430085e7257c9c044ba
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-02-17 17:19:15 +0200

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+  for (i = 0; i < 10; i++)
+  {
+    for (j = 0; j < 10; j++)
+    {
+      for (k = i; k < 10; k++)
+      {
+        for (l = j + 1; l < 10; l++)
+        {
+          putchar(i + '0');
+          putchar(j + '0');
+          putchar(' ');
+          putchar(k + '0');
+          putchar(l + '0');
+          if (i < 9 || j < 8)
+          {
+            putchar(',');
+            putchar(' ');
+          }
+        }
+      }
+    }
+  }
+
+	putchar('\n');
+
+	return (0);
+}
