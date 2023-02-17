ba6db31a031fc48cbc607559689aa8202d0d2f26
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-02-17 14:57:08 +0200

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (n % 10 > 5)
 	{
-		printf("greater than 5 \n");
+		printf("greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("0 \n");
+		printf("0\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0 \n");
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
