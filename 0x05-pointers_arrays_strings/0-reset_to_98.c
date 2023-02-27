00a82213474b5f73e65bbefbbaf565477f8f5d28
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-02-27 20:20:05 +0200

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
