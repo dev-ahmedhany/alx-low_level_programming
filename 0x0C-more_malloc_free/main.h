711617e583f3284288ac2006bef3990238ce9357
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-03-16 13:53:32 +0200

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,9 @@ int *array_range(int min, int max);
 #include <stdio.h>
 #include <stdlib.h>
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 
 #endif
