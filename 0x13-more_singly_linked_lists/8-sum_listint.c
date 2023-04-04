f6b03f9e67b77d7536e61e9f2bdf16b735fd18ae
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-04 17:30:34 +0200

    betty

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,12 +7,12 @@
  */
 int sum_listint(listint_t *head)
 {
-  int sum = 0;
+	int sum = 0;
 
-  while (head != NULL)
-  {
-    sum += head->n;
-    head = head->next;
-  }
-  return (sum);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
