d18cc61cca856799bf01a400830695671c44e2f8
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-05 01:48:29 +0200

    fix null

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *tmp2;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 	tmp = *head;
 	if (index == 0)
