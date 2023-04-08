e64b14e7f54c3946a1f831cc92f99d57ebbb3d27
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-09 00:15:02 +0200

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,25 +12,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n_read, n_written;
-    char *buf;
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-        return (0);
-    n_read = read(fd, buf, letters);
-    if (n_read == -1)
-        return (0);
-    n_written = write(STDOUT_FILENO, buf, n_read);
-    if (n_written == -1)
-        return (0);
-    free(buf);
-    close(fd);
-    return (n_written);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+		return (0);
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1)
+		return (0);
+	free(buf);
+	close(fd);
+	return (n_written);
 }
