042640676745a9d14226fea1071da671c396cc8f
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-08 20:44:18 +0200

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int num = 0;
+	unsigned int num = 0;
 
-  if (b == NULL)
-    return (0);
-  while (*b != '\0')
-  {
-    if (*b != '0' && *b != '1')
-      return (0);
-    num <<= 1;
-    if (*b == '1')
-      num += 1;
-    b++;
-  }
-  return (num);
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		num <<= 1;
+		if (*b == '1')
+			num += 1;
+		b++;
+	}
+	return (num);
 }
