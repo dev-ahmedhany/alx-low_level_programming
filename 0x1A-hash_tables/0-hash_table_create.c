bdecc067289b3930a1ac4f67d53e2db497e5bbea
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-06-11 11:47:06 +0300

    betty2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
