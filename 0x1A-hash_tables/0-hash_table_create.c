4541b53f2bb7e10afe9622fd658dd2dfff14faa2
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-06-11 11:46:27 +0300

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,13 @@
 #include "hash_tables.h"
 
-hash_table_t *hash_table_create(unsigned long int size){
+hash_table_t *hash_table_create(unsigned long int size)
+{
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		return NULL;
-	return new_table;
+		return (NULL);
+	return (new_table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 #include <stdlib.h>
 
 /**
@@ -30,3 +33,5 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
