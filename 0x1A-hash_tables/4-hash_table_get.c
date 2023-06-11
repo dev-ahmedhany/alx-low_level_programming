1118405e48db178896407bde203fce9e420d295a
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-06-11 12:27:10 +0300

    betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ *
+ * Return: value associated with the element, or NULL if key couldn't be found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
